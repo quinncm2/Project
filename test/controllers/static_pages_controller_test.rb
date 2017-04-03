05fe28cc11f4b3f485419c24158dc064f29c7c2f
Clint Quinn <quinncm2@email.uscupstate.edu>
2017-04-03 22:16:41 +0000

    Add a Static Pages controller2

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,5 +5,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_home_url
     assert_response :success
   end
+  
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+
+  test "should get news" do
+    get static_pages_news_url
+    assert_response :success
+  end
 
 end
