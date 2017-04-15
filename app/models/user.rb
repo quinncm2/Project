367388fce9dc281ec2201b7b5599ea16bc48dcce
Clint Quinn <quinncm2@email.uscupstate.edu>
2017-04-15 18:27:11 +0000

    finished chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,42 @@
 class User < ApplicationRecord
-      before_save { self.email = email.downcase }
-      validates :name, presence: true, length: { maximum: 60 }
-      VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-      validates :email, presence: true, length: { maximum: 260 },
+  attr_accessor :remember_token
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 60 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 260 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
-      has_secure_password
-      validates :password, length: { minimum: 6 }
-      
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+ 
+  class << self     
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+  
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  def authenticated?(remember_token)
+    if remember_digest.nil?
+       false
+    else
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+  end
+  
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
+  
