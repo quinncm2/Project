7e6aa4261f6db5c2f838b1743d3baa47d22bb4bd
Clint Quinn <quinncm2@email.uscupstate.edu>
2017-04-17 20:13:37 +0000

    final commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   
   def index
      @users = User.paginate(page: params[:page])
@@ -20,9 +20,12 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-       log_in @user
-       flash[:success] = "Welcome to Palidin PC!"
-       redirect_to @user
+    if @user.save
+      log_in @user
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
   
   def edit
@@ -39,7 +42,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-
+    
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
